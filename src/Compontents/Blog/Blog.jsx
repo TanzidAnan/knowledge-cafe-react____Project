38740e5678eaff86381477829c059536a3442570
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-22 01:09:02 +0600

    react style

--- src/Compontents/Blog/Blog.jsx
+++ src/Compontents/Blog/Blog.jsx
@@ -1,15 +1,36 @@
 import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
+    const { title, cover, reading_time, author, author_img,posted_date,hashtags } = blog
     console.log(blog)
     return (
-        <div>
-            
+        <div className='px-3'>
+            <img className='w-full' src={cover} alt={`cover pictcher ${title}`} />
+            <div className='flex justify-between items-center mt-5'>
+                <div className='flex gap-3'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div>
+                        <h4 className='text-lg'>{author}</h4>
+                        <p className='text-lg'>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <h4>{reading_time} min read</h4>
+                    
+                </div>
+
+            </div>
+            <h1 className='text-4xl font-semibold'>{title}</h1>
+            <p>
+                        {
+                            hashtags.map((hase,idx) => <span key={idx}><a href=""> #{hase}</a></span>)
+                        }
+                    </p>
         </div>
     );
 };
 
-Blog.propTypes ={
-    blog:PropTypes.object.isRequired
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired
 }
 
 export default Blog;
