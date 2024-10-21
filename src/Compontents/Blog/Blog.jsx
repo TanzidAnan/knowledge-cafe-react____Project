d88b3bb0951b8b328cb0e51ca8c7accaf9f75e6a
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-22 01:25:59 +0600

    react icone

--- src/Compontents/Blog/Blog.jsx
+++ src/Compontents/Blog/Blog.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
+import { CiBookmark } from "react-icons/ci";
+
 const Blog = ({ blog }) => {
     const { title, cover, reading_time, author, author_img,posted_date,hashtags } = blog
     console.log(blog)
     return (
         <div className='px-3'>
             <img className='w-full' src={cover} alt={`cover pictcher ${title}`} />
-            <div className='flex justify-between items-center mt-5'>
+            <div className='flex justify-between items-center mt-5 mb-3'>
                 <div className='flex gap-3'>
                     <img className='w-14' src={author_img} alt="" />
                     <div>
@@ -13,8 +15,9 @@ const Blog = ({ blog }) => {
                         <p className='text-lg'>{posted_date}</p>
                     </div>
                 </div>
-                <div>
+                <div className='flex gap-3'>
                     <h4>{reading_time} min read</h4>
+                    <button className='text-red-500 text-2xl'> <CiBookmark /></button>
                     
                 </div>
 
