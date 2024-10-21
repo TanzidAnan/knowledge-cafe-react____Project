8205939daa54a54f83a8ce78e237161a0d2b57fe
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-22 00:16:32 +0600

    proptypes blog compontent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './Compontents/Blogs/Blogs'
+import BookMarks from './Compontents/BookMarks/BookMarks'
 import Header from './Compontents/Header/Header'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className='md:flex w-[80%] mx-auto'>
       <Blogs></Blogs>
+      <BookMarks></BookMarks>
+      </div>
     </>
   )
 }
--- src/Compontents/Header/Header.jsx
+++ src/Compontents/Header/Header.jsx
@@ -2,9 +2,9 @@ import imgs from '../../assets/profile.png'
 import './Header.css'
 const Header = () => {
     return (
-        <div className='header-content border-b-2 pb-3'>
+        <div className='header-content border-b-2 pb-3 items-center'>
             <div>
-            <h1 className='text-4xl font-bold'>knowledge-cafe</h1>
+            <h1 className='text-2xl md:text-4xl font-bold'>knowledge-cafe</h1>
             </div>
             <div>
                 <img src={imgs} alt="" />
