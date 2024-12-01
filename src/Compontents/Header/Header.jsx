02aedab2eac5d6264e3486743a6f6461886042e9
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-01 21:35:38 +0600

    logo style

--- src/Compontents/Header/Header.jsx
+++ src/Compontents/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='header-content border-b-2 pb-3 items-center'>
             <div>
-            <h1 className='text-2xl md:text-4xl font-bold'>knowledge-cafe</h1>
+            <h1 className='text-2xl md:text-4xl font-bold text-gray-700'>knowledge-cafe</h1>
             </div>
             <div>
                 <img src={imgs} alt="" />
