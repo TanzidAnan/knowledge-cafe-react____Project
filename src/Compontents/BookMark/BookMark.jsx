7c96c62fb1cc7efa0d306b9c1a72eaece2fac9bd
TanzidAnan <tanzid.anan125@gmail.com>
2024-12-01 21:39:53 +0600

    title style

--- src/Compontents/BookMark/BookMark.jsx
+++ src/Compontents/BookMark/BookMark.jsx
@@ -4,7 +4,7 @@ const BookMark = ({bookMarks}) => {
     console.log(title)
     return (
         <div className="bg-slate-300 p-5 m-4 rounded-md">
-            <h3 className="text-2xl font-semibold">{title}</h3>
+            <h3 className="text-2xl font-semibold text-green-500">{title}</h3>
         </div>
     );
 };
